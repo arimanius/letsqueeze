7b4d87d853800c8dc28639a6129c13bdb06d83fb
Pouya Esmaili <poesmaili@gmail.com>
2022-12-24 22:30:19 +0330

    Fix rest apis

--- app/src/main/java/edu/arimanius/letsqueeze/data/http/dto/GetQuestionsResponse.kt
+++ app/src/main/java/edu/arimanius/letsqueeze/data/http/dto/GetQuestionsResponse.kt
@@ -5,5 +5,6 @@ import com.fasterxml.jackson.annotation.JsonProperty
 data class GetQuestionsResponse(
     @JsonProperty("response_code")
     val responseCode: Int = 0,
+    @JsonProperty("results")
     val results: List<QuestionResponse>
 )
--- app/src/main/java/edu/arimanius/letsqueeze/data/http/dto/QuestionResponse.kt
+++ app/src/main/java/edu/arimanius/letsqueeze/data/http/dto/QuestionResponse.kt
@@ -3,9 +3,13 @@ package edu.arimanius.letsqueeze.data.http.dto
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class QuestionResponse(
+    @JsonProperty("category")
     val category: String = "",
+    @JsonProperty("type")
     val type: String = "",
+    @JsonProperty("difficulty")
     val difficulty: String = "",
+    @JsonProperty("question")
     val question: String = "",
     @JsonProperty("correct_answer")
     val correctAnswer: String = "",
