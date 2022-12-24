bb31fe54428fb55138f508714abacf597824adda
Nazanin Azarian <badi.mojgan@gmail.com>
2022-12-24 17:31:08 +0330

    Fix play button

--- app/src/main/java/edu/arimanius/letsqueeze/ui/home/HomeFragment.kt
+++ app/src/main/java/edu/arimanius/letsqueeze/ui/home/HomeFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import edu.arimanius.letsqueeze.MainActivity
 import edu.arimanius.letsqueeze.R
+import edu.arimanius.letsqueeze.data.LetsQueezeDatabase
 import edu.arimanius.letsqueeze.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -18,12 +21,32 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
        _binding = FragmentHomeBinding.inflate(inflater,container, false)
-        val view = binding.root
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         binding.play.setOnClickListener{
-            findNavController().navigate(R.id.action_homeFragment_to_registerFragment)
+            val loggedInUser = LetsQueezeDatabase.getInstance(requireContext()).userDao().getLoggedInUser()
+            loggedInUser.observe(viewLifecycleOwner){
+                if (it == null){
+                    val appContext = context?.applicationContext?: return@observe
+                    Toast.makeText(appContext,R.string.login_prompt, Toast.LENGTH_LONG).show()
+                }else{
+                    findNavController().navigate(R.id.action_homeFragment_to_queezeFragment)
+                }
+            }
         }
-        return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+        (activity as MainActivity).isHome = true
+    }
+
+    override fun onStop() {
+        super.onStop()
+        (activity as MainActivity).isHome = false
     }
 
     override fun onDestroyView() {
