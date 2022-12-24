546483bacd3346fe02898ce484f981460026ccb3
Pouya Esmaili <poesmaili@gmail.com>
2022-12-25 02:01:48 +0330

    Add adapter in queeze result fragment

--- app/src/main/java/edu/arimanius/letsqueeze/ui/result/QueezeResultFragment.kt
+++ app/src/main/java/edu/arimanius/letsqueeze/ui/result/QueezeResultFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import edu.arimanius.letsqueeze.R
 import edu.arimanius.letsqueeze.databinding.FragmentQueezeBinding
 import edu.arimanius.letsqueeze.databinding.FragmentQueezeResultBinding
@@ -22,5 +25,12 @@ class QueezeResultFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val adapter = QuestionListAdapter(requireContext())
+        val recyclerView = view.findViewById<RecyclerView>(R.id.queeze_result_list)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+    }
 }
