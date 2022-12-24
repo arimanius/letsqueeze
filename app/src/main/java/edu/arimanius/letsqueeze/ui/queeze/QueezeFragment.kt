48902165322848b3f41352cd675495511d455def
Nazanin Azarian <badi.mojgan@gmail.com>
2022-12-24 17:41:41 +0330

    Add view binding to queeze fragment

---
+++ app/src/main/java/edu/arimanius/letsqueeze/ui/queeze/QueezeFragment.kt
@@ -0,0 +1,27 @@
+package edu.arimanius.letsqueeze.ui.queeze
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import edu.arimanius.letsqueeze.R
+import edu.arimanius.letsqueeze.databinding.FragmentQueezeBinding
+
+class QueezeFragment : Fragment() {
+    private var _binding: FragmentQueezeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentQueezeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
